0c76cbb9224c289e1447b301443d39ad8451da77
Charles <whitfield.cw@gmail.com>
2015-09-13 22:05:35 -0700

    add exercise 1-8, counting blanks and newlines

---
+++ blankcount.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+  int c, count;
+
+  count = 0;
+  while ((c = getchar()) != EOF)
+    if ((c == ' ') || (c == '\n'))
+      ++count;
+    printf("%d\n", count);
+}
